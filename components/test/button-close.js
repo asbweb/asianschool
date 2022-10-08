113b201f597acf81e62e72a36115a28be73436e9
asbweb <115213332+asbweb@users.noreply.github.com>
2022-10-08 19:04:56 +0530

    Delete components/test directory

--- components/test/button-close.js
+++
@@ -1,9 +0,0 @@
-import { Icon } from "@iconify/react"; 
-
-export default function CloseButton({ onClick }) {
-  return ( 
-      <button className="button-control right" type="button" onClick={onClick}> 
-        <Icon icon="mdi:close-circle" />
-      </button> 
-  );
-}
