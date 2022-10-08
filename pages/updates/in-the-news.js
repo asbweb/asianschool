c172714475b80be2a15ca79569ce360ea079a803
asbweb <115213332+asbweb@users.noreply.github.com>
2022-10-08 14:46:08 +0530

    Add files via upload

--- pages/updates/in-the-news.js
+++ pages/updates/in-the-news.js
@@ -1,25 +1,59 @@
+import { SITE_NAME, SITE_FAVICON, SITE_DESCRIPTION,  imgblurDataURL, HOME_OG_IMAGE_URL } from "../../lib/constants";
+import React, { useState } from "react";
 import Head from "next/head"; 
 import Container from "react-bootstrap/Container"; 
-import AsbGallery from "../../components/display-items/gallery";
+// import AsbGallery from "../../components/display-items/gallery";
+import ImageMosaic from "../../components/lightbox-gallery/image-mosaic"; 
+import ImageLightbox from "../../components/lightbox-gallery/image-lightbox";
 import images from "../../data/in-the-news.json";
 
 export default function InTheNews() {
+  const [isOpen, setOpen] = useState(false);
+  const [currentImageIndex, setCurrentIndex] = useState(0);
+
   return (
-    <div>
+    <>
       <Head>
-        <title>Asian Scool Bharain</title>
-        <meta name="description" content="Asian Scool Bharain" />
-        <link rel="icon" href="/images/asbfavicon.png" />
+        <title>{ SITE_NAME } </title>
+        <meta name="description" content={SITE_DESCRIPTION} />
+        <link rel="icon" href={SITE_FAVICON} />
       </Head>
 
       <main>
       <h2 className="page-title">In The News</h2> 
         <section className="my-5">
           <Container>  
-            <AsbGallery images={images.inthenews}/> 
+
+          <div className="my-5">
+            <ImageMosaic 
+              images={images.map((image) => ({
+                src: image.url,
+                title: image.title,
+                width: image.width, 
+                height: image.height,
+              }))}
+              handleClick={(e, { index }) => {
+                setCurrentIndex(index);
+                setOpen(true);
+              }}
+            />
+
+            <ImageLightbox
+              currentImageIndex={currentImageIndex}
+              setCurrentIndex={setCurrentIndex}
+              isOpen={isOpen}
+              onClose={() => setOpen(false)}
+              images={images.map((image) => ({
+                src: image.url,
+                title: image.title,
+                width: image.width, 
+                height: image.height,
+              }))}
+            />
+          </div>
           </Container>
         </section>
       </main>
-    </div>
+    </>
   );
 }
