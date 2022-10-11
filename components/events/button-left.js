a7ddfb5e6b3ed40ee515dc25691cfb18ef4567d4
asbweb <115213332+asbweb@users.noreply.github.com>
2022-10-11 10:26:07 +0530

    Delete components directory

--- components/events/button-left.js
+++
@@ -1,9 +0,0 @@
-import { Icon } from "@iconify/react";
-
-export default function LeftButton({ onClick }) {
-  return ( 
-      <button className="button-control left" type="button" onClick={onClick}> 
-        <Icon icon="mdi:chevron-left"/>
-      </button> 
-  );
-}
