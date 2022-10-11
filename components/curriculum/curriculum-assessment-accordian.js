3011b2f36fc4b5cd7ec0c17c813047707f25ed39
asbweb <115213332+asbweb@users.noreply.github.com>
2022-10-11 12:45:54 +0530

    Add files via upload

--- components/curriculum/curriculum-assessment-accordian.js
+++ components/curriculum/curriculum-assessment-accordian.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react"; 
+import React, { useState } from "react";
 import Image from "next/future/image";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Accordion from "react-bootstrap/Accordion";
 import { useAccordionButton } from "react-bootstrap/AccordionButton";
-import { Icon } from "@iconify/react";  
+import { Icon } from "@iconify/react";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import SmallRoundIconButton from "../buttons/small-round-icon-button"
 
 function CustomToggle({ children, eventKey }) {
-
   const [isActive, setIsActive] = useState(false);
   const handleClick = useAccordionButton(eventKey, () =>
     setIsActive(!isActive)
@@ -48,20 +49,41 @@ export default function CurriculumAssessmentAccordian({ weight, title, url }) {
         </Card.Header>
         <Accordion.Collapse eventKey={weight}>
           <Card.Body>
-            <Container className="py-5">
-     
-            <Image
-                  className="img-fluid p-0"
-                  src={url} 
-                  alt=""
-                  layout="intrinsic"
-                  width={1920} 
-                  height={1080}
-                />
+            <Container className="py-3">
+              <TransformWrapper
+                initialScale={1}
+                initialPositionX={0}
+                initialPositionY={0}
+                wheel={{ activationKeys: ['Control', 'Shift'] }}
+              >
+                {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                  <>
+                <div className="d-flex justify-content-end">
+                
+                <SmallRoundIconButton btnicon="bi:zoom-in" iconwidth={15} onClick={() => zoomIn()}/>
+                <SmallRoundIconButton btnicon="bi:zoom-out" iconwidth={15} onClick={() => zoomOut()}/>
+                <SmallRoundIconButton btnicon="bi:x-lg" iconwidth={15} onClick={() => resetTransform()}/>
+                {/* <button onClick={() => zoomIn()}> +</button>
+                <button onClick={() => zoomOut()}>-</button>
+                <button onClick={() => resetTransform()}>x</button> */}
+                </div>
+                <TransformComponent>
+                  <Image
+                    className="img-fluid p-0"
+                    src={url}
+                    alt=""
+                    layout="intrinsic"
+                    width={1920}
+                    height={1080}
+                  />
+                </TransformComponent>
+                </>
+                )}
+              </TransformWrapper>
             </Container>
           </Card.Body>
         </Accordion.Collapse>
       </Card>
     </>
   );
-} 
+}
