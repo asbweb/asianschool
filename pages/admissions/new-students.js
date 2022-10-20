214cb2a33e3ccbeb1ec1b2bf9bcd56a105820ec6
asbweb <115213332+asbweb@users.noreply.github.com>
2022-10-20 18:34:36 +0530

    Add files via upload

---
+++ pages/admissions/new-students.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import {
+  SITE_NAME,
+  SITE_FAVICON,
+  SITE_DESCRIPTION,
+  imgblurDataURL,
+  HOME_OG_IMAGE_URL,
+} from "../../lib/constants";
+import Head from "next/head";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
+import NewStudentRulesAndRecommendations from "@new-students/new-student-rules-and-recommendations";
+import NewStudentTransport from "@new-students/new-student-transport";
+import NewStudentUniform from "@new-students/new-student-uniform";
+import NewStudentSchoolTimings from "@new-students/new-student-school-timings";
+import CustomToggle from "@buttons/custom-toggle"
+
+export default function NewStudents() {
+  const [activeEventKey, setActiveEventKey] = useState(0);
+  return (
+    <div>
+      <Head>
+        {/* <title>{SITE_NAME} | New Students </title> */}
+        <title> Asian Scool Bahrain |  New Students </title>
+        <meta name="description" content={SITE_DESCRIPTION} />
+        <link rel="icon" href={SITE_FAVICON} />
+      </Head>
+
+      <main>
+        <h2 className="page-title">New Students</h2>
+        <section className="my-5">
+          <Accordion
+            activeeventkey={activeEventKey}
+            onToggle={setActiveEventKey}
+          >
+            <Card className="rounded-0">
+              <Card.Header className="bg-asb-accent text-uppercase">
+                <CustomToggle eventKey="0">Uniform</CustomToggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <Container fluid="md">
+                    <NewStudentUniform />
+                  </Container>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+
+            <Card className="rounded-0">
+              <Card.Header className="bg-asb-accent text-uppercase">
+                <CustomToggle eventKey="1">Transport</CustomToggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>
+                  <Container fluid="md">
+                    <NewStudentTransport />
+                  </Container>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+
+            <Card className="rounded-0">
+              <Card.Header className="bg-asb-accent text-uppercase">
+                <CustomToggle eventKey="2">
+                  Rules And Recommendations
+                </CustomToggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="2">
+                <Card.Body>
+                  <Container fluid="md">
+                    <NewStudentRulesAndRecommendations />
+                  </Container>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+
+            <Card className="rounded-0">
+              <Card.Header className="bg-asb-accent text-uppercase">
+                <CustomToggle eventKey="3">School Timings</CustomToggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="3">
+                <Card.Body>
+                  <Container fluid="md">
+                    <NewStudentSchoolTimings />
+                  </Container>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        </section>
+      </main>
+    </div>
+  );
+}
---
+++ pages/admissions/procedure.js
@@ -0,0 +1,354 @@
+import {
+  SITE_NAME,
+  SITE_FAVICON,
+  SITE_DESCRIPTION,
+  imgblurDataURL,
+  HOME_OG_IMAGE_URL,
+} from "../../lib/constants";
+import Head from "next/head";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import AdmissionSteps from "@admission-procedure/admission-steps"; 
+import AdmissionChecklist from "@admission-procedure/admission-checklist";
+import AdmissionParentPortal from "@admission-procedure/admission-parent-portal"; 
+
+import RichTextBlock from "@display-items/rich-text-block";
+
+export default function Procedure({
+  KGG1Step1,
+  KGG1Step2and3,
+  KGG1Step4to8,
+  G2AboveStep1,
+  G2AboveStep2and3,
+  G2AboveStep4to8,
+  KGGrade1Checklist,
+  Grade2AboveChecklist,
+  InfoforemailAdmissions,
+  AuthTC,
+}) {
+  const item = KGGrade1Checklist.map((a, index) => (
+    <p key={index}>{a.checklistItem}</p>
+  ));
+
+  console.log(item);
+
+  return (
+    <div>
+      <Head> 
+        <title> Asian Scool Bahrain | Admissions Procedure </title>
+        <meta name="description" content={SITE_DESCRIPTION} />
+        <link rel="icon" href={SITE_FAVICON} />
+      </Head>
+
+      <main>
+        <h2 className="page-title">Admissions Procedure</h2>
+        <section className="my-5 ">
+          <Container>
+            <Tabs
+              defaultActiveKey="kg-grade1"
+              id="uncontrolled-tab-example"
+              className="mx-auto"
+              justify
+            >
+              <Tab
+                eventKey="kg-grade1"
+                title="KG & GRADE 1"
+                className="homemenu "
+              >
+                <Row className="py-5 mx-0 bg-asb-accent">
+                  <Col>
+                    <div className="step-container">
+                      <p className="fw-bold mb-4">
+                        Before applying for admission, please make sure that ...
+                      </p>
+                      {KGGrade1Checklist.map((a, index) => (
+                        <AdmissionChecklist
+                          key={index}
+                          description={a.description}
+                        />
+                      ))}
+                    </div>
+
+                    {KGG1Step1.map((a, index) => (
+                      <AdmissionSteps
+                        key={index}
+                        steps={a.steps}
+                        description={a.description}
+                      />
+                    ))}
+ 
+                    <div className="step-container bg-light border border-asb-main">
+                      {InfoforemailAdmissions.map((a, index) => (
+                        <RichTextBlock
+                          key={index}
+                          title={a.title}
+                          description={a.description}
+                        />
+                      ))}
+                    </div>
+                    {KGG1Step2and3.map((a, index) => (
+                      <AdmissionSteps
+                        key={index}
+                        steps={a.steps}
+                        description={a.description}
+                      />
+                    ))}
+                    <AdmissionParentPortal />
+                    {KGG1Step4to8.map((a, index) => (
+                      <AdmissionSteps
+                        key={index}
+                        steps={a.steps}
+                        description={a.description}
+                      />
+                    ))}
+ 
+                    <div className="step-container bg-light border border-asb-main">
+                      {AuthTC.map((a, index) => (
+                        <RichTextBlock
+                          key={index}
+                          title={a.title}
+                          description={a.description}
+                        />
+                      ))}
+                    </div>
+                  </Col>
+                </Row>
+              </Tab>
+
+              <Tab eventKey="grade2andabove" title="GRADE 2 & ABOVE">
+                <Row className="py-5 mx-0 bg-asb-accent">
+                  <Col>
+                    <div className="step-container">
+                      <p className="fw-bold mb-4">
+                        Before applying for admission, please make sure that ...
+                      </p>
+                      {Grade2AboveChecklist.map((a, index) => (
+                        <AdmissionChecklist
+                          key={index}
+                          description={a.description}
+                        />
+                      ))}
+                    </div>
+
+                    {G2AboveStep1.map((a, index) => (
+                      <AdmissionSteps
+                        key={index}
+                        steps={a.steps}
+                        description={a.description}
+                      />
+                    ))} 
+                    <div className="step-container bg-light border border-asb-main">
+                      {InfoforemailAdmissions.map((a, index) => (
+                        <RichTextBlock
+                          key={index}
+                          title={a.title}
+                          description={a.description}
+                        />
+                      ))}
+                    </div>
+
+                    {G2AboveStep2and3.map((a, index) => (
+                      <AdmissionSteps
+                        key={index}
+                        steps={a.steps}
+                        description={a.description}
+                      />
+                    ))}
+                    <AdmissionParentPortal />
+                    {G2AboveStep4to8.map((a, index) => (
+                      <AdmissionSteps
+                        key={index}
+                        steps={a.steps}
+                        description={a.description}
+                      />
+                    ))}
+ 
+                    <div className="step-container bg-light border border-asb-main">
+                      {AuthTC.map((a, index) => (
+                        <RichTextBlock
+                          key={index}
+                          title={a.title}
+                          description={a.description}
+                        />
+                      ))}
+                    </div>
+                  </Col>
+                </Row>
+              </Tab>
+            </Tabs>
+          </Container>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const result = await fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master`,
+
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.CONTENTFUL_DELIVERY_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `query {
+          KGG1Step1: admissionProcedureStepsCollection(
+            where: { grades_contains_all: "KG and Grade 1", steps: "Step 1" }
+          ) {
+            items {
+              grades
+              steps
+              description {
+                json
+              }
+            }
+          }
+          KGG1Step2and3: admissionProcedureStepsCollection(
+            where: { grades_contains_all: "KG and Grade 1" }
+            skip: 1
+            limit: 2
+            order: steps_ASC
+          ) {
+            items {
+              grades
+              steps
+              description {
+                json
+              }
+            }
+          }
+          KGG1Step4to8: admissionProcedureStepsCollection(
+            where: { grades_contains_all: "KG and Grade 1" }
+            order: steps_ASC
+            skip: 3
+          ) {
+            items {
+              grades
+              steps
+              description {
+                json
+              }
+            }
+          }
+          G2AboveStep1: admissionProcedureStepsCollection(
+            where: { grades_contains_all: "Grade 2 and Above", steps: "Step 1" }
+          ) {
+            items {
+              grades
+              steps
+              description {
+                json
+              }
+            }
+          }
+          G2AboveStep2and3: admissionProcedureStepsCollection(
+            where: { grades_contains_all: "Grade 2 and Above" }
+            skip: 1
+            limit: 2
+            order: steps_ASC
+          ) {
+            items {
+              grades
+              steps
+              description {
+                json
+              }
+            }
+          }
+          G2AboveStep4to8: admissionProcedureStepsCollection(
+            where: { grades_contains_all: "Grade 2 and Above" }
+            order: steps_ASC
+            skip: 3
+          ) {
+            items {
+              grades
+              steps
+              description {
+                json
+              }
+            }
+          }
+          KGGrade1Checklist: admissionProcedureChecklistCollection(where: {title: "KG and Grade 1"} ) {
+            items{
+              title
+              checklistItem
+              list
+              description {
+                json
+              }
+            }
+          }
+          Grade2AboveChecklist: admissionProcedureChecklistCollection(where: {title: "Grade 2 and Above"} ) {
+            items{
+              title
+              checklistItem
+              list
+              description {
+                json
+              }
+            }
+          }
+          InfoforemailAdmissions: contentCollection(
+            where: { title: "Info for email to  Admissions " }
+          ) {
+            items {
+              title
+              description {
+                json
+              }
+            }
+          }
+          AuthTC: contentCollection(
+            where: { title: "Authentications required  TC" }
+          ) {
+            items {
+              title
+              description {
+                json
+              }
+            }
+          }
+        }
+        `,
+      }),
+    }
+  );
+
+  if (!result.ok) {
+    console.error(result);
+    return {};
+  }
+
+  const { data } = await result.json();
+  const KGG1Step1 = data.KGG1Step1.items;
+  const KGG1Step2and3 = data.KGG1Step2and3.items;
+  const KGG1Step4to8 = data.KGG1Step4to8.items;
+  const G2AboveStep1 = data.G2AboveStep1.items;
+  const G2AboveStep2and3 = data.G2AboveStep2and3.items;
+  const G2AboveStep4to8 = data.G2AboveStep4to8.items;
+  const KGGrade1Checklist = data.KGGrade1Checklist.items;
+  const Grade2AboveChecklist = data.Grade2AboveChecklist.items;
+  const InfoforemailAdmissions = data.InfoforemailAdmissions.items;
+  const AuthTC = data.AuthTC.items;
+
+  return {
+    props: {
+      KGG1Step1,
+      KGG1Step2and3,
+      KGG1Step4to8,
+      G2AboveStep1,
+      G2AboveStep2and3,
+      G2AboveStep4to8,
+      KGGrade1Checklist,
+      Grade2AboveChecklist,
+      InfoforemailAdmissions,
+      AuthTC,
+    },
+  };
+}
