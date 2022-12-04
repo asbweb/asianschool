36da18c03fbf593e7ff3fdaac8030a6549faa405
asbweb <115213332+asbweb@users.noreply.github.com>
2022-12-04 13:13:15 +0530

    Add files via upload

--- components/admission-procedure/admission-parent-portal.js
+++ components/admission-procedure/admission-parent-portal.js
@@ -12,7 +12,14 @@ export default function AdmissionParentPortal() {
         <Row className="my-1">
           <Col md={6} className="text-center my-3">
             <strong>For new students with siblings at ASB</strong>
-            <Link href="/pdf/admission-instructions/Online-Admission-Process-for-Siblings-Applications.pdf">
+
+            <h6 className="fw-light text-start mb-4">
+              If you have a child currently studying at The Asian School,
+              Please read the instructional document AND LOGIN to the Parent
+              Portal
+            </h6>
+            <Link href="/pdf/admission-instructions/Online-Admission-Process-for-Siblings-Applications.pdf
+            /pdf/admission-instructions/Online-Admission-Process-for-General-Applications.pdf">
               <a target="_blank" rel="noopener noreferrer">
                 <DownloadButton btnicon="">
                   <h6 className="fw-6 mb-0">
@@ -26,13 +33,33 @@ export default function AdmissionParentPortal() {
                 </DownloadButton>
               </a>
             </Link>
+
+            <div className="mx-auto my-3 text-center">
+              <a
+                href="http://portal.asianschool.bh/ASBDC/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <RoundIconButton btnicon="fa-solid:door-open" iconwidth="36">
+                  Parent Portal Login
+                </RoundIconButton>
+              </a>
+            </div>
           </Col>
+
           <Col md={6} className="text-center my-3">
             <strong>For new students with no siblings at ASB</strong>
+          
+            <h6 className="fw-light text-start">
+              If you are applying to The Asian School for the first time OR DO
+              NOT have a child currently studying at The Asian School, Please
+              read the instructional document AND Login to the Admission Portal 
+            </h6>
+
             <Link href="/pdf/admission-instructions/Online-Admission-Process-for-General-Applications.pdf">
               <a target="_blank" rel="noopener noreferrer">
                 <DownloadButton btnicon="">
-                  <h6 className="fw-6 mb-0">
+                  <h6 className="mb-0">
                     <Icon
                       icon="material-symbols:download-rounded"
                       width={24}
@@ -43,10 +70,29 @@ export default function AdmissionParentPortal() {
                 </DownloadButton>
               </a>
             </Link>
+
+            <div className="mx-auto my-3 text-center">
+              <a
+                href="http://asbdc.ethdigitalcampus.com/RegistrationPortal/forms/Utility/Login.jsp"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <RoundIconButton btnicon="fa-solid:door-open" iconwidth="36">
+                  Parent Portal Registration
+                </RoundIconButton>
+              </a>
+              <h6 className="fw-light text-start">
+              (Please enter your details first in ‘Create an Account’ to get
+              your User Id and Password and then continue to fill the form for
+              entering the child’s details)
+            </h6>
+            </div>
+
           </Col>
         </Row>
       </div>
-      <Row className="my-5">
+
+      {/* <Row className="my-5">
         <Col md={3} className="d-flex justify-content-center">
           <a
             href="http://portal.asianschool.bh/ASBDC/"
@@ -62,7 +108,7 @@ export default function AdmissionParentPortal() {
           Create an Account using your own details, then continue to fill the
           form with your child’s details.
         </Col>
-      </Row>
+      </Row> */}
     </>
   );
 }
