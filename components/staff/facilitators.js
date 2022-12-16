78d89d5326c738764bc63c36c94feb04ae8314ba
asbweb <115213332+asbweb@users.noreply.github.com>
2022-12-16 19:24:46 +0530

    Add files via upload

--- components/staff/facilitators.js
+++ components/staff/facilitators.js
@@ -3,7 +3,7 @@ import Image from "next/future/image";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 
-export default function Facilitators({ profilPicture, name, role, qualification }) {
+export default function Facilitators({ profilPicture, name, role, qualifications }) {
   return (
     <>
       <Col md={4}>
@@ -22,7 +22,7 @@ export default function Facilitators({ profilPicture, name, role, qualification 
               />
               <p className="text-center text-asb-secondary mb-0 fw-bold">{name}</p>
               <p className="text-center text-asb-secondary mb-0 fs-6">{role}</p>
-              <p className="text-center text-asb-secondary mb-0 fs-6">{qualification}</p>
+              <h6 className="text-center text-asb-secondary mb-0 fw-light">{qualifications}</h6>
             </div>
           </Card.Body>
         </Card>
