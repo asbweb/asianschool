549ecbe937e0b8d92eaad91ba080a4b55a73e3c4
asbweb <115213332+asbweb@users.noreply.github.com>
2023-01-17 12:05:50 +0530

    Update newsletter-item.js

--- components/display-items/newsletter-item.js
+++ components/display-items/newsletter-item.js
@@ -1,16 +1,39 @@
-import Link from "next/link"; 
+import Link from "next/link";
 import DownloadButton from "@buttons/download-button";
 
-export default function NewsletterItem({ title, subtitle, URL, btnicon, btntext }) {
+export default function NewsletterItem({
+  title,
+  subtitle,
+  URL,
+  url,
+  link,
+  btnicon,
+  btntext,
+}) {
   return (
     <>
       <h5>{title}</h5>
       <h6 className="mb-0">{subtitle}</h6>
-      <Link href={URL}>
+      {/* <Link href={URL}>
         <a className="resource-link" target="_blank" rel="noopener noreferrer">
           <DownloadButton btnicon={btnicon}>{btntext}</DownloadButton>
         </a>
-      </Link>
+      </Link> */}
+
+      <div>
+        {url ? (
+          <Link href={url.url}>
+            <a target="_blank" rel="noopener noreferrer">
+              <DownloadButton btnicon={btnicon}>{btntext}</DownloadButton>
+            </a>
+          </Link>
+        ) : null}
+        {link ? (
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <DownloadButton btnicon={btnicon}>{btntext}</DownloadButton>
+          </a>
+        ) : null}
+      </div>
     </>
   );
 }
