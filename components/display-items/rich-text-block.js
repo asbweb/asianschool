e8f78eda56e068c59f724c0531576ab1a9793dba
asbweb <115213332+asbweb@users.noreply.github.com>
2023-01-24 19:32:42 +0530

    Delete components directory

--- components/display-items/rich-text-block.js
+++
@@ -1,9 +0,0 @@
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-
-export default function RichTextBlock({ description }) {
-  return (
-    <>
-      {documentToReactComponents(description.json)}
-    </>
-  );
-}
