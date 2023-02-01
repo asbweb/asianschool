7aa0d59edc620d25f7b9d83b2b388a7f049074b5
asbweb <115213332+asbweb@users.noreply.github.com>
2023-02-01 16:32:57 +0530

    Add files via upload

--- pages/updates/school-calendar.js
+++ pages/updates/school-calendar.js
@@ -17,7 +17,9 @@ export default function Admission() {
         <section className="my-5">
           <Container>
             <iframe
-              src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23D47A0C&ctz=Asia%2FQatar&src=aW5mb0Bhc2lhbnNjaG9vbC5iaA&color=%23039BE5" 
+              // src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23D47A0C&ctz=Asia%2FQatar&src=aW5mb0Bhc2lhbnNjaG9vbC5iaA&color=%23039BE5" 
+              src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23D47A0C&ctz=Asia%2FQatar&src=info%40asianschool.bh&color=%23039BE5" 
+              
               style={{ borderWidth: "0" }}
               width="100%"
               height="800"
