30ef2bfeb52436209811539a1b7b35dbd5520be4
asbweb <115213332+asbweb@users.noreply.github.com>
2023-02-03 12:32:41 +0300

    Update profile.js

--- pages/about-us/profile.js
+++ pages/about-us/profile.js
@@ -43,16 +43,14 @@ export default function Profile({
               with the approval of the Ministry of Education, Kingdom of Bahrain. The institution was upgraded to the Secondary level in July 1992 
               and Senior Secondary level in December 2016. The school is affiliated to the Central Board of Secondary Education (CBSE), New Delhi 
               and has a strength of around 4500 students from Kindergarten to Grade 12.
-              <br>
-                The school is located in a sprawling 10.75 acre campus at Tubli. The school building has a built-up area of 16,000 sqm. with 133 classrooms. 
-                The school is also home to the 2000 sqm. multi-purpose Dr. Abdul Kalam Memorial Auditorium, one of the biggest in the Kingdom of Bahrain. 
-                The school also has a regulation size football ground and a basketball and tennis court built to international standards. 
-              </br>
-              <br>
-                The school has sent more than 20 batches of students to the All India Secondary School Certificate Examination conducted by the CBSE 
-                and all the batches have graduated with excellent results.
-              </br>  
-            </p>
+             
+              The school is located in a sprawling 10.75 acre campus at Tubli. The school building has a built-up area of 16,000 sqm. with 133 classrooms. 
+              The school is also home to the 2000 sqm. multi-purpose Dr. Abdul Kalam Memorial Auditorium, one of the biggest in the Kingdom of Bahrain. 
+              The school also has a regulation size football ground and a basketball and tennis court built to international standards. 
+              
+              The school has sent more than 20 batches of students to the All India Secondary School Certificate Examination conducted by the CBSE 
+              and all the batches have graduated with excellent results.
+           </p>
           </Col>
         </Container>
       </section>
