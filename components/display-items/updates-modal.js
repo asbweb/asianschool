166b0dba9d906701fcff70af044eff809e4756ae
asbweb <115213332+asbweb@users.noreply.github.com>
2023-02-14 10:42:28 +0530

    Update updates-modal.js

--- components/display-items/updates-modal.js
+++ components/display-items/updates-modal.js
@@ -11,7 +11,7 @@ export default function UpdatesModalTest({
   btnIcon, 
 }) {
   const [show, setShow] = useState(false);
-  useEffect(() => setShow(true), []);
+  useEffect(() => setShow(false), []);
   const handleClose = () => setShow(false); 
 
   return (
