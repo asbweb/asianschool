204cbe223eb4b36d296045c4829f502d4cdf5172
asbweb <115213332+asbweb@users.noreply.github.com>
2023-02-14 09:00:13 +0530

    Delete components directory

--- components/lightbox-gallery/button-right.js
+++
@@ -1,9 +0,0 @@
-import { Icon } from "@iconify/react"; 
-
-export default function RightButton({ onClick }) {
-  return ( 
-      <button className="button-control right" type="button" onClick={onClick}> 
-        <Icon icon="mdi:chevron-right" />
-      </button> 
-  );
-}
