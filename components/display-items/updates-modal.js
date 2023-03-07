b58fe5b55b3e7ff5454fc504c3d36391d01667d6
asbweb <115213332+asbweb@users.noreply.github.com>
2023-03-07 08:56:50 +0300

    Add files via upload

--- components/display-items/updates-modal.js
+++ components/display-items/updates-modal.js
@@ -2,39 +2,55 @@ import Link from "next/link";
 import DownloadButton from "@buttons/download-button";
 import { useEffect, useState } from "react";
 import Modal from "react-bootstrap/Modal";
-import RichTextBlock from "./rich-text-block"; 
+import RichTextBlock from "./rich-text-block";
 
 export default function UpdatesModal({
   title,
+  display,
   description,
+  displayBtn,
   btnText,
-  btnIcon, 
+  url,
+  link,
 }) {
   const [show, setShow] = useState(false);
-  useEffect(() => setShow(false), []);
-  const handleClose = () => setShow(false); 
+  useEffect(() => setShow(true), []);
+  const handleClose = () => setShow(false);
 
   return (
     <>
-      <Modal size="lg" show={show} onHide={handleClose} centered>
-        <Modal.Header className="border-0">
-          <Modal.Title className="fs-5 page-title text-center m-0">
-            {title}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <RichTextBlock description={description} />
-        </Modal.Body>
-        <Modal.Footer className="border-0 mt-0 pt-0">
-          <DownloadButton
-            className="mt-0"
-            onClick={handleClose}
-            btnicon={btnIcon}
-          >
-            {btnText}
-          </DownloadButton>
-        </Modal.Footer>
-      </Modal>
+      {display ? (
+        <Modal size="lg" show={show} onHide={handleClose} centered>
+          <Modal.Header className="border-0">
+            <Modal.Title className="fs-5 page-title text-center m-0">
+              {title}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <RichTextBlock description={description} />
+            <div className={displayBtn}>
+              {url ? (
+                <Link href={url.url} target="_blank" rel="noopener noreferrer" >
+                  <DownloadButton >{btnText}</DownloadButton>
+                </Link>
+              ) : null}
+              {link ? (
+                <a href={link} target="_blank" rel="noopener noreferrer" >
+                  <DownloadButton >{btnText}</DownloadButton>
+                </a>
+              ) : null}
+            </div>
+          </Modal.Body>
+          <Modal.Footer className="border-0 mt-0 pt-0">
+            <DownloadButton
+              className="mt-0"
+              onClick={handleClose} 
+            > 
+              Close
+            </DownloadButton>
+          </Modal.Footer>
+        </Modal>
+      ) : null}
     </>
   );
 }
