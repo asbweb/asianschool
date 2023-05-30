9aa53bf6ac446f636eb4da8037683fa24da2c2e1
asbweb <115213332+asbweb@users.noreply.github.com>
2023-05-30 13:18:13 +0300

    Add files via upload

--- components/display-items/home-video.js
+++ components/display-items/home-video.js
@@ -2,13 +2,11 @@ export default function HomeVideo() {
   return (
     <>
       <div className="mx-auto my-5 ratio ratio-16x9">
-        <iframe 
-          // width="560"
-          // height="480"
+        <iframe
           src="https://www.youtube.com/embed/zch89gbDf3o"
           title="The Asian School video"
-          allow="accelerometer; autoplay; fullscreen"
-          allowfullscreen
+          allow="accelerometer; autoplay; fullscreen" 
+          allowFullScreen
         ></iframe>
       </div>
     </>
