6762b08b869fa8eafff39534e9f879f4e640f63e
asbweb <115213332+asbweb@users.noreply.github.com>
2023-06-08 07:41:44 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
-
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ["images.ctfassets.net"],
     formats: ['image/avif', 'image/webp'],
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
   }
 }
 
